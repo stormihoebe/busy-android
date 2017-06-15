b3839a5b1e0d918d809a9551ee7f80b80074744c
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-15 16:06:28 -0700

    bind views in new user activity and implement view on click listener

--- app/src/main/java/com/stormhoebe/busy/NewUserActivity.java
+++ app/src/main/java/com/stormhoebe/busy/NewUserActivity.java
@@ -1,13 +1,40 @@
 package com.stormhoebe.busy;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class NewUserActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewUserActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Bind(R.id.typeEditText) EditText mTypeEditText;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.imageEditText) EditText mImageEditText;
+    @Bind(R.id.continueButton) Button mContinueButton;
+
+    private String mType;
+    private String mLocation;
+    private String mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+        ButterKnife.bind(this);
+
+        mContinueButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mContinueButton) {
+
+        }
+
     }
 }
