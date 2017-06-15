34af020482461399fa41a158e134adc5af4e9a55
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-15 11:24:01 -0700

    add sign in with password function to login activity

--- app/src/main/java/com/stormhoebe/busy/LoginActivity.java
+++ app/src/main/java/com/stormhoebe/busy/LoginActivity.java
@@ -1,11 +1,19 @@
 package com.stormhoebe.busy;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -13,6 +21,10 @@ import butterknife.ButterKnife;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.signUpbutton) Button mSignUpButton;
     @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         mSignUpButton.setOnClickListener(this);
         mLoginButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
     @Override
     public void onClick(View v) {
@@ -29,8 +43,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
             startActivity(intent);        }
         if (v == mLoginButton) {
-
+            loginWithPassword();
         }
+
+    }
+    private void loginWithPassword() {
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")) {
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")) {
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
 
     }
 }
