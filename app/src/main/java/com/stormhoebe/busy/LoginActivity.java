ec520509ef568a4ad70f171aff43eaa5f26c839e
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-15 09:41:53 -0700

    add on click listeners to login activity

--- app/src/main/java/com/stormhoebe/busy/LoginActivity.java
+++ app/src/main/java/com/stormhoebe/busy/LoginActivity.java
@@ -1,13 +1,36 @@
 package com.stormhoebe.busy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.signUpbutton) Button mSignUpButton;
+    @Bind(R.id.loginButton) Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mSignUpButton.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mSignUpButton) {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);        }
+        if (v == mLoginButton) {
+
+        }
+
     }
 }
