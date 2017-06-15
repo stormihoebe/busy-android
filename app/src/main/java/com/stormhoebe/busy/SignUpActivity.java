002baf27a622a25aed01d342dc6e91836d4a3c59
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-15 09:33:41 -0700

    add spinner to sign up activity with string array resource of industries, set on click listeners on sign up activity

--- app/src/main/java/com/stormhoebe/busy/SignUpActivity.java
+++ app/src/main/java/com/stormhoebe/busy/SignUpActivity.java
@@ -1,13 +1,48 @@
 package com.stormhoebe.busy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.industrySpinner) Spinner mIndustrySpinner;
+    @Bind(R.id.signUpbutton) Button mSignUpButton;
+    @Bind(R.id.signInTextView) TextView mSignInTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mSignUpButton.setOnClickListener(this);
+        mSignInTextView.setOnClickListener(this);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.industries_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mIndustrySpinner.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mSignUpButton) {
+            Log.d("SIGNUP BUTTON", "is working");
+        }
+        if (v == mSignInTextView) {
+            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
