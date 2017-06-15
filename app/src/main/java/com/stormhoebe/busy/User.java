7bc7a93619e1b16417dafe18c5423c50c3a822a7
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-15 13:40:39 -0700

    add constructor, getters and setter methods for User class

--- app/src/main/java/com/stormhoebe/busy/User.java
+++ app/src/main/java/com/stormhoebe/busy/User.java
@@ -1,6 +1,7 @@
 package com.stormhoebe.busy;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Guest on 6/15/17.
@@ -10,8 +11,8 @@ public class User {
     String name;
     String email;
     String industry;
-    String[] needs;
-    String[] offers;
+    ArrayList<String> needs;
+    ArrayList<String> offers;
     String image;
     String location;
     String id;
@@ -22,4 +23,47 @@ public class User {
         this.industry = industry;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void addNeed(String need) {
+        this.needs.add(need);
+    }
+
+    public void addOffer(String offer) {
+        this.offers.add(offer);
+    }
 }
